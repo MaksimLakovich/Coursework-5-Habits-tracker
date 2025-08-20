3b823e8d1225affa5c0f019c077d0ecb105d0020
Maksim Lakovich <maks.lakovich@gmail.com>
2025-08-20 10:09:22 +0300

    Создана админка AppUserAdmin (UserAdmin) в users/managers.py.

--- users/admin.py
+++ users/admin.py
@@ -6,7 +6,7 @@ from users.models import AppUser
 
 @admin.register(AppUser)
 class AppUserAdmin(UserAdmin):
-    """Настройка отображения модели *AppUser (Пользователь)* в админке."""
+    """Настройка отображения модели *AppUser* (Пользователь) в админке."""
 
     list_display = ("email", "first_name", "last_name", "is_staff", "is_superuser")
     search_fields = ("email", "first_name", "last_name")
