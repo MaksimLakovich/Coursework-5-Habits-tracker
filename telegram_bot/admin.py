6c7bdb4cd11ad927a1af0b2e8de1c53812a97082
Maksim Lakovich <maks.lakovich@gmail.com>
2025-08-26 13:47:35 +0300

    1) Создал новую модель TelegramProfile. 2) Зарегистрировал ее в админке TelegramProfileAdmin. 3) Выполнил миграции. 4) Обновлен файл README.md

--- telegram_bot/admin.py
+++ telegram_bot/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from telegram_bot.models import TelegramProfile
+
+
+@admin.register(TelegramProfile)
+class TelegramProfileAdmin(admin.ModelAdmin):
+    list_display = ("id", "app_user", "telegram_chat_id", "telegram_user_id", "created_at", "updated_at",)
+    list_filter = ("app_user", "telegram_chat_id",)
+    search_fields = ("app_user__email", "telegram_chat_id", "telegram_user_id",)
+    ordering = ("app_user",)
--- telegram_bot/models.py
+++ telegram_bot/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+from config import settings
+from habits.models import TimeStampedModel
+
+
+class TelegramProfile(TimeStampedModel):
+    """Модель *TelegramProfile* представляет телеграм-профиль пользователя в приложении."""
+
+    app_user = models.OneToOneField(
+        to=settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="owned_telegram_profile",
+        verbose_name="Пользователь приложения:",
+        help_text="Укажите пользователя приложения",
+    )
+    telegram_chat_id = models.CharField(
+        max_length=100,
+        unique=True,
+        null=True,
+        blank=True,
+        verbose_name="Telegram ChatID:",
+        help_text="Укажите ChatID пользователя приложения в Telegram (куда шлём сообщения)",
+    )
+    telegram_user_id = models.CharField(
+        max_length=100,
+        null=True,
+        blank=True,
+        verbose_name="Telegram UserID:",
+        help_text="Укажите UserID пользователя приложения в Telegram (опционально)",
+    )
+
+    def __str__(self):
+        """Метод определяет строковое представление объекта. Полезно для отображения объектов в админке/консоли."""
+        return f"{self.app_user} (Telegram ChatID: {self.telegram_chat_id})"
+
+    class Meta:
+        verbose_name = "Телеграм-профиль"
+        verbose_name_plural = "Телеграм-профили"
+        ordering = ["app_user"]
