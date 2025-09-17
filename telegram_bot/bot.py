c712f8ac5afba7c80a32fd60512c739c3817b201
Maksim Lakovich <maks.lakovich@gmail.com>
2025-09-17 12:02:19 +0300

    Исправлены ошибки после запуска проверки flake8

--- telegram_bot/bot.py
+++ telegram_bot/bot.py
@@ -239,4 +239,3 @@ if __name__ == "__main__":
         nest_asyncio.apply()
         loop = asyncio.get_event_loop()
         loop.run_until_complete(main())
-
--- users/urls.py
+++ users/urls.py
@@ -11,5 +11,9 @@ urlpatterns = [
     path("register/", UserViewSetAPIView.as_view({"post": "create"}), name="user-register"),
     path("users/<int:pk>/", UserViewSetAPIView.as_view({"get": "retrieve"}), name="user-detail"),
     path("users/<int:pk>/update/", UserViewSetAPIView.as_view({"patch": "partial_update"}), name="user-update"),
-    path("users/<int:pk>/set_password/", UserViewSetAPIView.as_view({"post": "user_set_password"}), name="user-set-password"),
+    path(
+        "users/<int:pk>/set_password/",
+        UserViewSetAPIView.as_view({"post": "user_set_password"}),
+        name="user-set-password"
+    ),
 ]
