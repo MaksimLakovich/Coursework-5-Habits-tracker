bbcafe94d7d285be1c2facf8f189e3a9dc867799
Maksim Lakovich <maks.lakovich@gmail.com>
2025-09-17 23:08:32 +0300

    Исправлена проблема с CSRF_TRUSTED_ORIGINS

--- config/settings.py
+++ config/settings.py
@@ -196,9 +196,18 @@ CORS_ALLOWED_ORIGINS = [
 ]
 
 # Для работы CSRF с кросс-доменными запросами (POST, PUT, DELETE)
+# 1) ЧТО ЭТО?
+# Если используется нестандартный порт (например, http://127.0.0.1:8081/admin/ вместо http://127.0.0.1:8000/admin/),
+# то Django будет не доверять адресу http://127.0.0.1:8081/admin/, так как источник будет не совпадать с
+# доверенным доменом из ALLOWED_HOSTS или CSRF_TRUSTED_ORIGINS и выдаст 403 CSRF verification failed.
+# Чтоб исключить ошибку нужно добавить параметр CSRF_TRUSTED_ORIGINS в settings.py и указывать в нем список
+# доверенных доменов с портами
+# 2) ДОП ПОЯСНЕНИЕ:
+# Django проверяет конфигурацию, и в CSRF_TRUSTED_ORIGINS должен быть СПИСОК и без пустых некорректных
+# данных, поэтому если не хардкодить тут и выносить в .ENV , то нужно писать код для создания списка без пустых
+# значений в конце.
 CSRF_TRUSTED_ORIGINS = [
-    'http://localhost:3000',
-    'https://habits-frontend.example.com',
+    origin for origin in os.getenv("CSRF_TRUSTED_ORIGINS", "").split(",") if origin
 ]
 
 # Запрещаем доступ для всех подряд (оставляем только из списка выше)
